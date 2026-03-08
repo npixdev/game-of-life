f92a346de59f3af3511a80f876e1847c913d704e
unknown <pramic.nc@gmail.com>
2026-03-08 15:37:42 +0100

    random table 0-1

--- test.c
+++ test.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int table[20][20] = {0};
     for (int i = 0; i < 20; i++) {
         for (int j = 0; j < 20; j++) {
+            table[i][j] = rand() % 2;
+        }
+    }
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
             printf("%d ", table[i][j]);
-        } printf("\n");
+        }
+        printf("\n");
     }
     return 0;
 }
+
