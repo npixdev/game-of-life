491dfd7c715f0b9d45ab2be781abed3b969031e2
unknown <pramic.nc@gmail.com>
2026-03-08 16:02:02 +0100

    first functionnal loop

--- test.c
+++ test.c
@@ -1,16 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int table[20][20] = {0};
+int random_table(int table[20][20]) {
     for (int i = 0; i < 20; i++) {
         for (int j = 0; j < 20; j++) {
             table[i][j] = rand() % 2;
         }
     }
+    return 0;
+}
+
+int main() {
+    int table[20][20] = {0};
+    int new_table[20][20] = {0};
+    random_table(table);
     for (int i = 0; i < 20; i++) {
         for (int j = 0; j < 20; j++) {
-            printf("%d ", table[i][j]);
+            int count = 0;
+            for (int di = -1; di < 2; di++) {
+                for (int dj = -1; dj <2; dj++) {
+                    if (di == 0 && dj == 0) {
+                        continue;
+                    }
+                    if (table[i+di][j+dj] == 1) {
+                        count++;
+                    }
+                }
+            }
+            if (table[i][j] == 1 && count == 2  || count ==3) {
+                new_table[i][j] = 1;
+            } else if (table[i][j] == 1 && count <2 || count > 3) {
+                new_table[i][j] == 0;
+            } else if (table[i][j] == 0 && count == 3) {
+                new_table[i][j] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
+            printf("%d ", new_table[i][j]);
         }
         printf("\n");
     }
